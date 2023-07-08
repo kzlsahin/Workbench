6f87cb17179bd77881e3958de84097de968b3e8b
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-07-08 21:38:18 +0300

    MetricUnits are added.

---
+++ MetricUnits/Cenimeter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISO_12217.Units
+{
+    public record struct Centimeter
+    {
+        public double Value { get; set; }
+        public Centimeter(double value)
+        {
+            Value = value;
+        }
+        public static implicit operator Centimeter(double value)
+        {
+            return new Centimeter(value);
+        }
+        public static implicit operator Centimeter(float value)
+        {
+            return new Centimeter(value);
+        }
+        public static implicit operator Centimeter(int value)
+        {
+            return new Centimeter(value);
+        }
+        public static implicit operator Centimeter(Meter cm)
+        {
+            return new Meter(cm.Value / 100);
+        }
+        public static implicit operator Centimeter(Milimeter mm)
+        {
+            return new Meter(mm.Value * 10);
+        }
+        public static Centimeter operator +(Centimeter a) => a;
+        public static Centimeter operator -(Centimeter a) => (-1) * a;
+        public static Centimeter operator *(Centimeter a, Centimeter b) => new Centimeter(a.Value * b.Value);
+        public static Centimeter operator *(double d, Centimeter a) => new Centimeter(d * a.Value);
+        public static Centimeter operator *(Centimeter a, double d) => new Centimeter(d * a.Value);
+        public static Centimeter operator /(Centimeter a, Centimeter b) => new Centimeter(a.Value / b.Value);
+        public static Centimeter operator /(double d, Centimeter a) => new Centimeter(d / a.Value);
+        public static Centimeter operator /(Centimeter a, double d) => new Centimeter(a.Value / d);
+        public static Centimeter operator +(Centimeter a, Centimeter b) => new Centimeter(a.Value + b.Value);
+        public static Centimeter operator +(double d, Centimeter a) => new Centimeter(d + a.Value);
+        public static Centimeter operator +(Centimeter a, double d) => new Centimeter(d + a.Value);
+        public static Centimeter operator -(Centimeter a, Centimeter b) => new Centimeter(a.Value - b.Value);
+        public static Centimeter operator -(double d, Centimeter a) => new Centimeter(d - a.Value);
+        public static Centimeter operator -(Centimeter a, double d) => new Centimeter(d - a.Value);
+
+        // **** when struct is defined as record these equality checks comes by default.
+        //public static bool operator ==(Centimeter a, Centimeter b) => a.Value == b.Value;
+        //public static bool operator !=(Centimeter a, Centimeter b) => a.Value != b.Value;
+        // ****
+        public static bool operator <(Centimeter a, Centimeter b) => a.Value < b.Value;
+        public static bool operator >(Centimeter a, Centimeter b) => a.Value > b.Value;
+        public static bool operator <=(Centimeter a, Centimeter b) => a.Value <= b.Value;
+        public static bool operator >=(Centimeter a, Centimeter b) => a.Value >= b.Value;
+    }
+}
---
+++ MetricUnits/Milimeter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISO_12217.Units
+{
+    public record struct Milimeter
+    {
+        public double Value { get; set; }
+        public Milimeter(double value)
+        {
+            Value = value;
+        }
+        public static implicit operator Milimeter(double value)
+        {
+            return new Milimeter(value);
+        }
+        public static implicit operator Milimeter(float value)
+        {
+            return new Milimeter(value);
+        }
+        public static implicit operator Milimeter(int value)
+        {
+            return new Milimeter(value);
+        }
+        public static implicit operator Milimeter(Centimeter cm)
+        {
+            return new Meter(cm.Value / 10);
+        }
+        public static implicit operator Milimeter(Meter mm)
+        {
+            return new Meter(mm.Value / 1000);
+        }
+        public static Milimeter operator +(Milimeter a) => a;
+        public static Milimeter operator -(Milimeter a) => (-1) * a;
+        public static Milimeter operator *(Milimeter a, Milimeter b) => new Milimeter(a.Value * b.Value);
+        public static Milimeter operator *(double d, Milimeter a) => new Milimeter(d * a.Value);
+        public static Milimeter operator *(Milimeter a, double d) => new Milimeter(d * a.Value);
+        public static Milimeter operator /(Milimeter a, Milimeter b) => new Milimeter(a.Value / b.Value);
+        public static Milimeter operator /(double d, Milimeter a) => new Milimeter(d / a.Value);
+        public static Milimeter operator /(Milimeter a, double d) => new Milimeter(a.Value / d);
+        public static Milimeter operator +(Milimeter a, Milimeter b) => new Milimeter(a.Value + b.Value);
+        public static Milimeter operator +(double d, Milimeter a) => new Milimeter(d + a.Value);
+        public static Milimeter operator +(Milimeter a, double d) => new Milimeter(d + a.Value);
+        public static Milimeter operator -(Milimeter a, Milimeter b) => new Milimeter(a.Value - b.Value);
+        public static Milimeter operator -(double d, Milimeter a) => new Milimeter(d - a.Value);
+        public static Milimeter operator -(Milimeter a, double d) => new Milimeter(d - a.Value);
+
+        // **** when struct is defined as record these equality checks comes by default.
+        //public static bool operator ==(Milimeter a, Milimeter b) => a.Value == b.Value;
+        //public static bool operator !=(Milimeter a, Milimeter b) => a.Value != b.Value;
+        // ****
+        public static bool operator <(Milimeter a, Milimeter b) => a.Value < b.Value;
+        public static bool operator >(Milimeter a, Milimeter b) => a.Value > b.Value;
+        public static bool operator <=(Milimeter a, Milimeter b) => a.Value <= b.Value;
+        public static bool operator >=(Milimeter a, Milimeter b) => a.Value >= b.Value;
+    }
+}
