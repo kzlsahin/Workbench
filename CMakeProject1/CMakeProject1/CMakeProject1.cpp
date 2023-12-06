1a9ba2522b0bba9ec2f5b2fbe82a01361c7eb5a8
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-12-06 15:44:37 +0300

    reference is added for the website

--- CMakeProject1/CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -1,5 +1,9 @@
 ﻿// CMakeProject1.cpp : Defines the entry point for the application.
 //
+/*
+* Code samples from:
+* https://www.nar-associates.com/nurbs/c_code.html#chapter2
+*/
 #include "CMakeProject1.h"
 
 using namespace std;
