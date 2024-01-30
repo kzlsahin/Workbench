2f5afdc5dd84444a9d1e172b8b8309c32ba9321f
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2024-01-30 17:00:21 +0300

    Project - Avalonia Project AvalonCrypter is added.

---
+++ FileEncrypter/AvalonCrypter/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AvalonCrypter.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ FileEncrypter/AvalonCrypter/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AvalonCrypter.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
